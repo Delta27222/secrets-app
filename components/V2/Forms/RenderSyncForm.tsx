5a5cddd0131202350bbbc0f75fad5c3da1153d8a
Delta27222 <angelgabriel.hernandez@gmail.com>
2025-06-04 16:17:39 -0400

    fix: correct import statement for useRenderActions in RenderSyncForm

--- components/V2/Forms/RenderSyncForm.tsx
+++ components/V2/Forms/RenderSyncForm.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import { Save } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useProjectEnvironments } from "@/hooks";
-import { useRenderActions } from "@/hooks/useRenderactions";
+import { useProjectEnvironments, useRenderActions } from "@/hooks";
 
 export function RenderSyncForm() {
   const didFetchRef = React.useRef(false);
