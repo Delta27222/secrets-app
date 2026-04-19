746f7206b3456fd2196c89ddaa320284545ff61f
angel.hernandez@sabatransporte.com <angelgabriel.hernandez@gmail.com>
2026-04-18 22:02:00 -0400

    refactor: update middleware matcher to include new routes for organizations and projects

--- middleware.ts
+++ middleware.ts
@@ -22,6 +22,6 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/secretos/:path*"],
+  matcher: ["/organizations/:path*", "/projects/:path*", "/"],
 }
 
